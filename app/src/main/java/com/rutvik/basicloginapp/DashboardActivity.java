c61310937348baadfffec31b9a46ef198e3de475
rutvikgondaliya <gondaliyarutvik281@gmail.com>
2021-11-29 22:40:49 +0530

    First Commit

--- app/src/main/java/com/rutvik/basicloginapp/DashboardActivity.java
+++ app/src/main/java/com/rutvik/basicloginapp/DashboardActivity.java
@@ -3,13 +3,6 @@ package com.rutvik.basicloginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class DashboardActivity extends AppCompatActivity {
 
