4bbaba090e6670935ff85252e3e47078993089ab
rutvikgondaliya <gondaliyarutvik281@gmail.com>
2021-11-29 22:26:54 +0530

    First Commit

---
+++ app/src/main/java/com/rutvik/basicloginapp/DashboardActivity.java
@@ -0,0 +1,27 @@
+package com.rutvik.basicloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    EditText secretCodeBox;
+    Button joinBtn, shareBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+
+    }
+}
